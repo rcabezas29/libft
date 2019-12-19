8da5e72f44b2178349803fe8b5ed43ad4cd96a43
Raúl Cabezas González <raul.29cg@gmail.com>
2019-12-19 09:06:34 +0100

    Suida libft a GH

---
+++ ft_strlcpy.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rcabezas <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/11/09 12:20:13 by rcabezas          #+#    #+#             */
+/*   Updated: 2019/12/10 15:36:33 by rcabezas         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t size)
+{
+	size_t srclen;
+
+	if (!(src && dst))
+		return (0);
+	srclen = ft_strlen(src);
+	if (srclen + 1 < size)
+	{
+		ft_memcpy(dst, src, srclen + 1);
+	}
+	else if (size != 0)
+	{
+		ft_memcpy(dst, src, size - 1);
+		dst[size - 1] = '\0';
+	}
+	return (srclen);
+}
---
+++ ft_strmapi.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rcabezas <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/11/14 20:00:42 by rcabezas          #+#    #+#             */
+/*   Updated: 2019/11/26 18:13:59 by rcabezas         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	char			*fstr;
+	unsigned int	i;
+	int				j;
+
+	i = 0;
+	if (!s)
+		return (0);
+	if (!(fstr = (char*)malloc(ft_strlen(s) + 1)))
+		return (0);
+	j = ft_strlen((char *)s);
+	while (s[i])
+	{
+		fstr[i] = f(i, s[i]);
+		i++;
+	}
+	fstr[i] = '\0';
+	return (fstr);
+}
