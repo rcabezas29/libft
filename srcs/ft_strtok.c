2e679ed740f605e6f493ec5e99c0f6d6b98fab4a
Raúl Cabezas González <raul.29cg@gmail.com>
2021-10-20 10:55:01 +0200

    Updated ft_strtok

--- srcs/ft_strtok.c
+++ srcs/ft_strtok.c
@@ -3,28 +3,29 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strtok.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rcabezas <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: rcabezas <rcabezas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/10/20 10:10:01 by rcabezas          #+#    #+#             */
-/*   Updated: 2021/10/20 10:12:01 by rcabezas         ###   ########.fr       */
+/*   Created: 2021/10/20 10:53:57 by rcabezas          #+#    #+#             */
+/*   Updated: 2021/10/20 10:54:16 by rcabezas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char    *ft_strtok(const char *str, char c)
+char	*ft_strtok(const char *str, char c)
 {
-    int     i;
-    char    *dest;
-    i = 0;
-    dest = ft_strdup(str);
-    while (str[i] != c)
-    {
-        dest[i] = str[i];
-        i++;
-    }
-    dest[i] = '\0';
+	int		i;
+	char	*dest;
+
+	i = 0;
+	dest = ft_strdup(str);
+	while (str[i] && str[i] != c)
+	{
+		dest[i] = str[i];
+		i++;
+	}
+	dest[i] = '\0';
 	if (!dest)
 		return (NULL);
-    return (dest);
+	return (dest);
 }
